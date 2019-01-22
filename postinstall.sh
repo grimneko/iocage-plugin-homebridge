5c58ee2dac61d66f9554bceffa30a6cb42390f1e
grimneko <github@grimneko.de>
2019-01-22 13:01:04 +0100

    Initial setup for the plugin repository

---
+++ postinstall.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# enable services
+sysrc -f /etc/rc.conf dbus_enable="YES"
+sysrc -f /etc/rc.conf avahi_daemon_enable="YES"
+
+# link dns_sd.h to the place where its expected
+ln -s /usr/local/include/avahi-compat-libdns_sd/dns_sd.h /usr/include/dns_sd.h
+
+# start services
+service dbus start
+service avahi-daemon start
+
+# begin install of homebridge and the config-ui
+npm install -g --unsafe-perm homebridge
+npm install -g --unsafe-perm homebridge-config-ui-x
+
+# install process manager to keep homebridge running / booting on iocage restarts
+# and provide the config ui with logs
+npm install -g pm2
+pm2 startup rcd
+
+# enable pm2 service
+sysrc -f /etc/rc.conf pm2_enable="YES"
+
+# setup pm2
+pm2 start homebridge -- -D
+pm2 save
---
+++ readme.md
@@ -0,0 +1,13 @@
+a basic homebridge plugin with the webui (homebridge-config-ui-x) setup.
+Please keep in mind to change the pin code, the mac and set a password for the admin.
+
+I want to thank the following webpages for providing me pointers and hints to make this
+possible:
+
+Homebridge @ Github for the basic guide to setup things on FreeNAS
+https://github.com/nfarina/homebridge/wiki/FreeNAS-11-Installation-using-a-Jail
+
+SmartApfel.de for providing information how to setup the webui
+https://smartapfel.de/homebridge/plugins-installieren/
+
+
